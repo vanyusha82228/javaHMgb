e676a29743ef3be183972f69b5b2e7409c231748
vanusha <ivan00.02mail.ru>
2023-03-14 23:17:04 +0300

    complit

--- hm4/task1.java
+++ hm4/task1.java
@@ -1,30 +1,32 @@
 package hm4;
+//Пусть дан LinkedList с несколькими элементами. Реализуйте метод(не void), который вернет “перевернутый” список
 import java.util.LinkedList;
 import java.util.Random;
 
 public class task1 {
     public static void main(String[] args) {
-        LinkedList<Integer> ll = new LinkedList<>();
-        randFillList(10, 100, ll);
-        LinkedList<Integer> newLL = returnReversedList(ll);
-
-        System.out.println(ll.toString());
-        System.out.println(newLL.toString());
-    }
-
-    public static LinkedList<Integer> returnReversedList(LinkedList<Integer> list) {
-        LinkedList<Integer> newList = new LinkedList<>();
-
-        for(int i = list.size(); i > 0; i--) newList.add(list.get(i - 1));
-
-        return newList;
-    }
-
-    public static void randFillList(int count, int maxValue, LinkedList<Integer> list) {
-        Random rand = new Random();
+        LinkedList<Integer> linkList = new LinkedList<>();
+        randNum(10, 50, linkList);
         
-        for (int i = 0; i < count; i++) {
-            list.add(rand.nextInt(maxValue));
-        }
+        LinkedList<Integer> newL = revL(linkList);
+        System.out.println(linkList.toString());
+        System.out.println(newL.toString());
+
     }
+
+    public static void randNum(int value, int maxValue, LinkedList<Integer> linkList) {
+        Random random = new Random();
+        for (int i = 0; i < value; i++) {
+            linkList.add(random.nextInt(maxValue));
+        }    
+    }
+
+    public static LinkedList<Integer> revL(LinkedList<Integer> linkList) {
+        LinkedList<Integer> newLinkList = new LinkedList<>();
+        for (int i = linkList.size(); i >0 ; i--) 
+            newLinkList.add(linkList.get(i-1));
+
+        return newLinkList;
+    }
+   
 }
--- hm4/task2.java
+++ hm4/task2.java
@@ -1,41 +1,51 @@
 package hm4;
-
+// Реализуйте очередь с помощью LinkedList со следующими методами:
+// enqueue() - помещает элемент в конец очереди,
+// dequeue() - возвращает первый элемент из очереди и удаляет его,
+// first() - возвращает первый элемент из очереди, не удаляя
 import java.util.LinkedList;
-import java.util.Random;
+import java.util.List;
+import java.util.Scanner;
+
 
 public class task2 {
     public static void main(String[] args) {
-        LinkedList<Integer> ll = new LinkedList<>();
-        randFillList(10, 100, ll);
+        Scanner sc =new Scanner(System.in);
+        LinkedList<Integer> linkList = new LinkedList<>();
+        while(true){
+            System.out.println("Введите что вы хотите делать(цифру от 1 до 3): ");
+            int num = sc.nextInt();
+            if(num==1){
+                System.out.println("Введите элемент: ");
+                int value = sc.nextInt();
+                enqueue(linkList, value);
+                System.out.println(linkList.toString());
+            }
+            else if(num==2){
+                System.out.println("возвращает первый элемент из очереди и удаляет его ");
+                dequeue(linkList);
+                System.out.println(linkList.toString());
+            }
 
-        System.out.println(ll);
-
-        enqueue(ll, 0);
-        System.out.println(ll);
-
-        System.out.println(dequeue(ll));
-        System.out.println(ll);
-
-        System.out.println(first(ll));
-    }
-
-    public static void enqueue(LinkedList<Integer> list, int value) {
-        list.add(value);
-    }
-
-    public static int dequeue(LinkedList<Integer> list) {
-        return list.removeFirst();
-    }
-
-    public static int first(LinkedList<Integer> list) {
-        return list.getFirst();
-    }
-
-    public static void randFillList(int count, int maxValue, LinkedList<Integer> list) {
-        Random rand = new Random();
-        
-        for (int i = 0; i < count; i++) {
-            list.add(rand.nextInt(maxValue));
+            else if(num==3){
+                System.out.println(" возвращает первый элемент из очереди, не удаляя. ");
+                int firstEl = first(linkList);
+                System.out.println(firstEl);
+                System.out.println(linkList.toString());
+            }
+            else 
+                System.out.println("eror");
         }
     }
+
+    public static void enqueue(LinkedList<Integer> linkList, int num) {
+        linkList.add(num);
+    }
+    public static void dequeue(LinkedList<Integer> linkList) {
+        linkList.removeFirst();
+    }
+    private static int first(LinkedList<Integer> linkList) {
+        return linkList.getFirst();
+    }
+ 
 }
