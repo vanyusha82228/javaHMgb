e951db40c436ea3783402f1305dce7148d4462e3
vanusha <ivan00.02mail.ru>
2023-03-18 12:41:28 +0300

    upgraed task1

--- hm5/task1.java
+++ hm5/task1.java
@@ -33,42 +33,45 @@ public class task1 {
     }
 
     public static void addCont(Map<String,ArrayList<String>> map, Scanner sc) {
-        ArrayList<String> numberList = new ArrayList<>();
         boolean examination = true;
-        while(examination){
-            System.out.println("Для введения нового контакта нажмите-1, для добавления номера в существующий жамите-2, для выхода в главное меню-0");
-            String value = sc.nextLine();
-                switch (value) {
-                    case "1":
-                        System.out.print("Введите фамилию:");
-                        String secondName = sc.nextLine();
-                        System.out.print("Введите введите номер:");
-                        String numberPhone = sc.nextLine();
-                        numberList.add(numberPhone);
-                        map.put(secondName, numberList);
-                        break;
-                    case "2":
-                        System.out.print("Введите фамилию:");
-                        String secondName1 = sc.nextLine();
-                        System.out.print("Введите введите номер:");
-                        String newNumberPhone = sc.nextLine();
+    while(examination){
+        System.out.println("Для введения нового контакта нажмите-1, для добавления номера в существующий жамите-2, для выхода в главное меню-0");
+        String value = sc.nextLine();
+            switch (value) {
+                case "1":
+                    System.out.print("Введите фамилию:");
+                    String secondName = sc.nextLine();
+                    System.out.print("Введите введите номер:");
+                    String numberPhone = sc.nextLine();
+                    ArrayList<String> numberList = new ArrayList<>();
+                    numberList.add(numberPhone);
+                    map.put(secondName, numberList);
+                    break;
+                case "2":
+                    System.out.print("Введите фамилию:");
+                    String secondName1 = sc.nextLine();
+                    System.out.print("Введите введите номер:");
+                    String newNumberPhone = sc.nextLine();
+                    if(map.containsKey(secondName1))
                         map.get(secondName1).add(newNumberPhone);
-                        break;
-                    case "0":
-                        examination = false;
-                        System.out.println("Вы в главном меню");
-                        break;
-                    default:
-                        System.out.println("Такой команды нет!");
-                }
+                    else
+                        System.out.println("Такого контакта нет, для добавления нового жамите-1");
+                        
+                    break;
+                case "0":
+                    examination = false;
+                    System.out.println("Вы в главном меню");
+                    break;
+                default:
+                    System.out.println("Такой команды нет!");
             }
         }
+    }
     
 
     public static void outputOfAllNumbers(Map<String,ArrayList<String>> map) {
         System.out.println();
-
-        for(Map.Entry<String,ArrayList<String>>entry : map.entrySet()){
+        for(Map.Entry<String,ArrayList<String>> entry : map.entrySet()){
             System.out.println("Фамилия: "+ entry.getKey() + "Телефоны: "+ entry.getValue());
         }
     }
